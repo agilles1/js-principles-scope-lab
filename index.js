2bb04fc8eae5b32e5d4d0c05c1594ee7153f4c01
agilles1 <adam.g.tpt@gmail.com>
2021-02-02 21:04:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = 'BOB'
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer
+}
+
+const leastFavoriteCustomer = 'Sophie'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Adam'
+}
